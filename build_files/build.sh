b18ec29b471cb5a9b54e2dd1f84a66b72df995df
Jeremy <j-em@users.noreply.github.com>
2026-07-02 04:08:48 -0400

    Update build script to install 'zed' instead of 'konsole'

--- build_files/build.sh
+++ build_files/build.sh
@@ -16,8 +16,10 @@ dnf5 -y install \
   https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
   https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
 
+dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release terra-gpg-keys
+
 # this installs a package from fedora repos
-dnf5 install -y tmux kitty konsole
+dnf5 install -y tmux kitty zed
 
 dnf5 install -y code-insiders
 
